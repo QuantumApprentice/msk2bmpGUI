0b9aeae29f6289acf5667b17f1b19af93e610a79
QuantumApprentice <quantumapprentice@gmail.com>
2022-03-03 00:52:07 -0800

    Started on the conversion process, but only produces white images right now

---
+++ msk2bmpGUI_C_C++_imgui_sdl2_tinyfiledialogs/B_Endian.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef B_ENDIAN_HPP
+#define B_ENDIAN_HPP
+
+#include <cstdint>
+#include <fstream>
+
+namespace B_Endian {
+	// Convert signed values
+	//int32_t read_32(std::istream& f);
+	//int16_t read_16(std::istream& f);
+	//int8_t  read_8 (std::istream& f);
+
+	// Convert unsigned values
+	uint32_t read_u32(std::istream& f);
+	uint16_t read_u16(std::istream& f);
+	uint8_t  read_u8 (std::istream& f);
+}
+
+#endif
--- msk2bmpGUI_C_C++_imgui_sdl2_tinyfiledialogs/FRM_Convert.cpp
+++ msk2bmpGUI_C_C++_imgui_sdl2_tinyfiledialogs/FRM_Convert.cpp
@@ -1,5 +1,9 @@
 #include "FRM_Convert.h"
+#include "B_Endian.h"
 #include <cstdint>
+#include <fstream>
+#include <vector>
+#include <SDL.h>
 
 #pragma pack(push, 1)
 typedef struct {
@@ -20,7 +24,74 @@ typedef struct {
 } FRM_Header;
 #pragma pack(pop)
 
-void FRM_Convert()
-{
+std::vector<SDL_Color> PaletteColors;
 
+bool loadPalette()
+{
+	std::ifstream f("color.pal", 
+		std::ios::in | std::ios::binary);
+	if (!f.is_open()) {
+		printf("Error opening color.pal.\n");
+		return false;
+	}
+	
+	uint8_t r, g, b;
+	PaletteColors.resize(256);
+	printf("Palette size: %d\n", PaletteColors.size());
+	for (int i = 0; i < PaletteColors.size(); i++)
+	{
+		r = B_Endian::read_u8(f);
+		g = B_Endian::read_u8(f);
+		b = B_Endian::read_u8(f);
+		printf("RGB: %d, %d, %d\n", r, g, b);
+		PaletteColors[i] = SDL_Color{ r, g, b };
+	}
+	return true;
+}
+
+
+SDL_Surface* FRM_Convert(SDL_Surface *surface)
+{
+	loadPalette();
+	struct rgba
+	{
+		uint8_t r;
+		uint8_t g;
+		uint8_t b;
+		uint8_t a;
+	} rgba_;
+
+	 SDL_Surface* Temp_Surface = SDL_CreateRGBSurface(0, surface->w, surface->h, 8, 0, 0, 0, 0);
+	for (int i = 0; i < (surface->w * surface->h); i++)
+	{
+		memcpy(&rgba_, (rgba*)surface->pixels + i, sizeof(rgba));
+		int w_smallest = INT_MAX;
+		int w_PaletteColor;
+		for (int j = 0; j < 256; j++)
+		{
+			int s = (rgba_.r - PaletteColors[j].r);
+			int t = (rgba_.g - PaletteColors[j].g);
+			int u = (rgba_.b - PaletteColors[j].b);
+			int v = (rgba_.a - PaletteColors[j].a);
+			s *= s;
+			t *= t;
+			u *= u;
+			v *= v;
+
+			int w = sqrt(s + t + u + v);
+
+			if (w < w_smallest) { 
+				w_smallest = w; 
+				w_PaletteColor = j;
+			}
+			printf("i: %d, j: %d \n", i, j);
+			//printf("stuv: %d %d %d %d", s, t, u, v);
+		}
+		((uint8_t*)Temp_Surface->pixels)[i]  = w_PaletteColor;
+		//printf("w_PaletteColor: %d\n", w_PaletteColor);
+	}
+	
+	SDL_SaveBMP_RW(Temp_Surface, SDL_RWFromFile("temp1.bmp", "wb"), 1);
+
+	return Temp_Surface;
 }
