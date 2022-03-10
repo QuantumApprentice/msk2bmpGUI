9a06bf71dd535c312197ae7699137698a0e9db07
QuantumApprentice <quantumapprentice@gmail.com>
2022-03-10 04:51:05 -0800

    can open multiple windows now, but for some reason the c_name string keeps getting corrupted? only most recent name is kept, previous opened file names are lost

--- msk2bmpGUI_C_C++_imgui_sdl2_tinyfiledialogs/Load_Files.h
+++ msk2bmpGUI_C_C++_imgui_sdl2_tinyfiledialogs/Load_Files.h
@@ -4,6 +4,8 @@ struct LF {
 	char * Opened_File;
 	char * c_name;
 	SDL_Surface* image;
+	SDL_Texture* Optimized_Texture = nullptr;
+	int texture_width = 0, texture_height = 0;
 
 	bool file_open_window;
 	char * FilterPattern1[2] = { "*.bmp" , "*.png" };
