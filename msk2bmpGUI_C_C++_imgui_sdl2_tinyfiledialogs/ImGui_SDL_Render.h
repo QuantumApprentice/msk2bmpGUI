a17fbfe01f29a09c991e37fcb90247a660ec9c28
QuantumApprentice <quantumapprentice@gmail.com>
2022-03-16 22:41:49 -0700

    My first attempt to convert stuff from SDL to OpenGL, but it turns out I can't use SDL renderer to render stuff unless I know the back end muuuuuch better than I do now.  Time to start learning OpenGL!

---
+++ msk2bmpGUI_C_C++_imgui_sdl2_tinyfiledialogs/ImGui_SDL_Render.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "imgui-docking/imgui.h"      // IMGUI_IMPL_API
+
+struct SDL_Renderer;
+
+IMGUI_IMPL_API bool     ImGui_ImplSDLRenderer_Init(SDL_Renderer* renderer);
+IMGUI_IMPL_API void     ImGui_ImplSDLRenderer_RenderDrawData(ImDrawData* draw_data);
