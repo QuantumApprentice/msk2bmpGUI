c1ae1369a2a178f5b91adb78c67461f3fc96b481
Adam Bryzak <git@adam.bryzak.net>
2024-06-11 23:09:03 +1000

    Expand crop_single_tile test to handle 4 ways the tile might be cropped, detect writing out of bounds and add coverage reporting when running the tests

--- build_linux.sh
+++ build_linux.sh
@@ -21,14 +21,14 @@ CC_ARGS=(-g "${INCLUDE_ARGS[@]}")
 CPP_ARGS=(-g "${INCLUDE_ARGS[@]}")
 
 if [[ "${1:-}" == "clean" ]]; then
-  rm -fr build
   shift
+  rm -fr build
 fi
 
 if [[ "${1:-}" == "release" ]]; then
+  shift
   CC_ARGS+=(-O3)
   CPP_ARGS+=(-O3)
-  shift
 fi
 
 mkdir -p build
@@ -45,15 +45,20 @@ c++ "${CPP_ARGS[@]}" -o build/QFO2Tool src/build_linux.cpp build/c_libs.o build/
 echo "Copying resources"
 cp -a src/resources build
 
+cov=0
+if [[ "${1:-}" == "coverage" ]]; then
+  shift
+  CPP_ARGS+=(--coverage)
+  cov=1
+fi
+
 if [[ "${1:-}" == "test" ]]; then
   shift
   cp -a test/test_resources build
   c++ "${CPP_ARGS[@]}" -o build/QFO2Tool_test test/test_tile_export.cpp build/c_libs.o build/cpp_libs.o
   echo "Running tests"
   (cd build && ./QFO2Tool_test) || echo 'Tests failed'
+  [[ "$cov" == 1 ]] && (mkdir -p build/coverage && gcov build/QFO2Tool_test-test_tile_export && mv -- *.gcov build/coverage) >/dev/null && echo "Coverage files generated from tests in build/coverage"
 fi
-
-echo "Removing build artifacts"
-rm build/*.o
 
 echo "Done"
--- test/test_tile_export.cpp
+++ test/test_tile_export.cpp
@@ -1,50 +1,55 @@
 #define main app_main
-#import "../src/build_linux.cpp"
+#include "../src/build_linux.cpp"
 #undef main
 
-#import <stdio.h>
-#import <assert.h>
+#include <stdio.h>
+#include <assert.h>
 
-#define FAIL 1
+// we might want to make the FAIL non-zero if we want multiple
+// types of failures, but doing !test_blah to check for failure
+// reads a little nicer so we'll only make that change if we do
+// have multiple things we care about how it failed
+#define FAIL 0
+#define PASS 1
 
-int main()
+#define TILE_W 80
+#define TILE_H 36
+
+int test_single_tile_crop(Surface* mask, uint8_t tile_buff_full[TILE_W*TILE_H*3], uint8_t frm_pxls[100 * 100], int frm_offset_x, int frm_offset_y)
 {
-  // yes, this leaks memory on failure, no, I don't really care, it's a test
-  Surface* surface = LoadFileAsRGBASurface("test_resources/tile_mask.png");
-  if (!surface) {
-    printf("Unable to load tile template\n");
-    return FAIL;
-  }
-  uint8_t tile_buff[80 * 36] = {0};
-  uint8_t frm_pxls[100 * 100] = {0};
-  memset(frm_pxls, 123, sizeof(frm_pxls));
-  crop_single_tile(100, 100, tile_buff, frm_pxls, -40, -10);
-  for (int x = 0; x < 80; x++)
+  memset(tile_buff_full, 0xCD, TILE_W*TILE_H);
+  memset(frm_pxls, 133, 100*100); // 133 = bright red
+  // this gives a full tile in every direction around the central tile
+  uint8_t* tile_buff = tile_buff_full + TILE_W*TILE_H;
+  memset(tile_buff, 0, TILE_W*TILE_H);
+  memset(tile_buff + TILE_W*TILE_H, 0xCD, TILE_W*TILE_H);
+  crop_single_tile(100, 100, tile_buff, frm_pxls, frm_offset_x, frm_offset_y);
+  for (int x = 0; x < TILE_W; x++)
   {
-    for (int y = 0; y < 36; y++)
+    for (int y = 0; y < TILE_H; y++)
     {
-      Color* surface_pixel = (Color*) (surface->pixels + y * surface->pitch + x * surface->channels);
-      uint8_t expected_value = 123;
+      Color* mask_pixel = (Color*) (mask->pixels + y * mask->pitch + x * mask->channels);
+      uint8_t expected_value = 133;
 
       // if the pixel is transparent in the mask,
       // it should be transparent in the tile
-      if (surface_pixel->a == 0) expected_value = 0;
+      if (mask_pixel->a == 0) expected_value = 0;
 
-      // if the pixel is "to the left" of the image,
+      // if the pixel is "to the left or right" of the image,
       // it should be transparent in the tile
-      if (x < 40) expected_value = 0;
+      if (x + frm_offset_x < 0 || x + frm_offset_x >= 100) expected_value = 0;
 
-      // if the pixel is "above" the image,
+      // if the pixel is "above or below" the image,
       // it should be transparent in the tile
-      if (y < 10) expected_value = 0;
+      if (y + frm_offset_y < 0 || y + frm_offset_y >= 100) expected_value = 0;
 
-      uint8_t actual_value = tile_buff[y * 80 + x];
+      uint8_t actual_value = tile_buff[y * TILE_W + x];
 
       if (actual_value != expected_value) {
         Palette* palette = load_palette_to_Palette("resources/palette/color.pal");
-        Surface* exported = Create8BitSurface(80, 36, palette);
+        Surface* exported = Create8BitSurface(TILE_W, TILE_H, palette);
+        memcpy(exported->pixels, tile_buff, TILE_W*TILE_H);
         exported = ConvertSurfaceToRGBA(exported);
-        memcpy(exported->pixels, tile_buff, sizeof(tile_buff));
         const char* filename = "test_resources/failed_tile_export.png";
         SaveSurfaceAsPNG(exported, filename);
         printf("Expected value %d at x:%d,y:%d, but we got %d. Wrote output to %s\n",
@@ -53,7 +58,58 @@ int main()
       }
     }
   }
-  FreeSurface(surface);
+  uint8_t* oob_check_1 = tile_buff_full;
+  uint8_t* oob_check_2 = tile_buff + TILE_W*TILE_H;
+  for (int i = 0; i < TILE_W*TILE_H; i++)
+  {
+    uint8_t* p;
+    if ((p = oob_check_1)[i] != 0xCD || (p = oob_check_2)[i] != 0xCD) {
+      printf("Detected out-of-bounds write at index %ld relative to the tile_buff pointer\n",
+        (int64_t)p - (int64_t)tile_buff);
+      return FAIL;
+    }
+  }
+  return PASS;
+}
+
+int test()
+{
+  // yes, this leaks memory on failure, no, I don't really care, it's a test
+  Surface* mask = LoadFileAsRGBASurface("test_resources/tile_mask.png");
+  if (!mask) {
+    printf("Unable to load tile template\n");
+    return FAIL;
+  }
+  int result = PASS;
+  uint8_t tile_buff_full[TILE_W*TILE_H*3]; // 3x as large so we can test for writing out of bounds
+  uint8_t frm_pxls[100 * 100];
+  if (!test_single_tile_crop(mask, tile_buff_full, frm_pxls, 10, 10)) {
+    printf("Full crop test failed\n");
+    result = FAIL;
+  }
+  if (!test_single_tile_crop(mask, tile_buff_full, frm_pxls, -40, -10)) {
+    printf("Top-left crop test failed\n");
+    result = FAIL;
+  }
+  if (!test_single_tile_crop(mask, tile_buff_full, frm_pxls, 60, -10)) {
+    printf("Top-right crop test failed\n");
+    result = FAIL;
+  }
+  if (!test_single_tile_crop(mask, tile_buff_full, frm_pxls, -40, 80)) {
+    printf("Bottom-left crop test failed\n");
+    result = FAIL;
+  }
+  if (!test_single_tile_crop(mask, tile_buff_full, frm_pxls, 60, 80)) {
+    printf("Bottom-right crop test failed\n");
+    result = FAIL;
+  }
+  FreeSurface(mask);
+  return result;
+}
+
+int main()
+{
+  if (!test()) return 1;
   printf("Tests passed\n");
   return 0;
 }
